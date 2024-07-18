8da1559b21865ca9cee12bfb00d728428424f072
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-07-18 16:57:06 +0100

    Implementations

---
+++ src/Infrastructure/Persistence/Repositories/JamaatRepository.cs
@@ -0,0 +1,42 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class JamaatRepository(AppDbContext _context) : IJamaatRepository
+    {
+        public async Task<Jamaat> Add(Jamaat jamaat)
+        {
+            await _context.Jamaats.AddAsync(jamaat);
+            return jamaat;
+        }
+
+        public async Task Delete(Jamaat jamaat)
+        {
+            _context.Jamaats.Remove(jamaat);
+        }
+
+        public async Task<Jamaat> Get(Expression<Func<Jamaat, bool>> expression)
+        {
+            return await _context.Jamaats.SingleOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<Jamaat>> GetAll(Expression<Func<Jamaat, bool>> expression)
+        {
+            return await _context.Jamaats.Where(expression).ToListAsync();
+        }
+
+        public async Task<Jamaat> Update(Jamaat jamaat)
+        {
+            _context.Jamaats.Update(jamaat);
+            return jamaat;
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/MemberLedgerRepository.cs
@@ -0,0 +1,42 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class MemberLedgerRepository(AppDbContext _context) : IMemberLedgerRepository
+    {
+        public async Task<MemberLedger> Add(MemberLedger memberLedger)
+        {
+            await _context.MemberLedgers.AddAsync(memberLedger);
+            return memberLedger;
+        }
+
+        public async Task Delete(MemberLedger memberLedger)
+        {
+            _context.MemberLedgers.Remove(memberLedger);
+        }
+
+        public async Task<MemberLedger> Get(Expression<Func<MemberLedger, bool>> expression)
+        {
+            return await _context.MemberLedgers.SingleOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<MemberLedger>> GetAll(Expression<Func<MemberLedger, bool>> expression)
+        {
+            return await _context.MemberLedgers.Where(expression).ToListAsync();
+        }
+
+        public async Task<MemberLedger> Update(MemberLedger memberLedger)
+        {
+            _context.MemberLedgers.Update(memberLedger);
+            return memberLedger;
+        }
+    }
+}
