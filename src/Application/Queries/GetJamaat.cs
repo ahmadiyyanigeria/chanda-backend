a3c748f9b63f21682b7c7adfe900bc3c34c825e2
YahyaOlalekan <mybluvedcreator@gmail.com>
2024-07-27 11:52:24 +0100

    implement GetJamaat

---
+++ src/Application/Queries/GetJamaat.cs
@@ -0,0 +1,32 @@
+﻿using Application.Repositories;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class GetJamaat
+    {
+        public record JamaatResponse(string Name, Guid CircuitId, string CreatedBy, string? ModifiedBy, DateTime CreatedOn, DateTime? ModifiedOn);
+        public record Query(Guid Id) : IRequest<JamaatResponse>;
+       
+        public class Handler : IRequestHandler<Query, JamaatResponse>
+        {
+            private readonly IJamaatRepository _jamaatRepository;
+            public Handler(IJamaatRepository jamaatRepository)
+            {
+                _jamaatRepository = jamaatRepository;
+            }
+            public async Task<JamaatResponse> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var jamaat = await _jamaatRepository.Get(j => j.Id == request.Id);
+                return jamaat.Adapt<JamaatResponse>();
+            }
+             
+        }
+    }
+}
