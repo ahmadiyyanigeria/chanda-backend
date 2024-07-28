2977c2982cae588f6fd871c2715404dd027b9e58
abdurrasheed199 <ayodejiabdulazeezabdurrasheed@gmail.com>
2024-07-28 09:55:02 +0100

    Implement get a ChandaType

---
+++ src/Application/Queries/GetChandaTypeAsync.cs
@@ -0,0 +1,46 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class GetChandaTypeAsync
+    {
+        
+        public class Query : IRequest<ChandaTypeResponse>
+        {
+            public Guid Id { get; set; }
+
+            public Query(Guid id)
+            {
+                Id = id;
+            }
+        }
+
+        
+        public class GetQueryHandler : IRequestHandler<Query, ChandaTypeResponse>
+        {
+            private readonly IChandaTypeRepository _chandaTypeRepository;
+
+            public GetQueryHandler(IChandaTypeRepository chandaTypeRepository)
+            {
+                _chandaTypeRepository = chandaTypeRepository;
+            }
+
+            public  async Task<ChandaTypeResponse> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var chanda =  await _chandaTypeRepository.GetByIdAsync(request.Id);
+                return chanda.Adapt<ChandaTypeResponse>();
+            }
+        }
+
+        public record ChandaTypeResponse(string Name,string Code,Guid IncomeAccountId, string Description, DateTime CreatedDate, DateTime? ModifiedDate);
+
+    }
+}
